3fff20bab3e5bb623a6e8775c50cc2c86cfa63d5
bob <hasnul14@gmail.com>
2022-10-17 20:24:27 +0800

    Deleted unusual documentation technique indicated by #Doc lines.

--- src/extract.py
+++ src/extract.py
@@ -4,8 +4,6 @@ from util import ratio
 from util import showImage, drawPerspective, drawBoundaries, drawContour, writedoc
 from util import randomColor
 
-
-
 import cv2
 import numpy as np
 
@@ -22,6 +20,7 @@ def largestContour(contours):
         if contour_area > largest[0]:
             largest = (contour_area, c)
     return largest[1]
+
 
 def ignoreContours(img,
                    contours,
@@ -71,7 +70,6 @@ def ignoreContours(img,
     return ret
 
 
-
 def extractBoards(img, w, h):
     """Extracts all boards from an image. This function applies perspective correction.
     :param img: source image
@@ -80,57 +78,21 @@ def extractBoards(img, w, h):
     :returns: a list the extracted board images
     """
     im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    ## Doc ##
-    #writeDocumentationImage(im_gray, "gray")
-    ## Doc ##
-
     (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    ## Doc ##
-    #writeDocumentationImage(im_bw, "bw")
-    ## Doc ##
-
     contours,hierarchy = cv2.findContours(im_bw,  cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
-
-    ## Doc ##
-    #doc_im_contour = cv2.cvtColor(im_gray, cv2.COLOR_GRAY2BGR)
-    #for c in contours:
-    #    c = np.squeeze(c,1)
-    #    drawContour(doc_im_contour, c, randomColor())
-    #writeDocumentationImage(doc_im_contour, "contours")
-    #doc_im_perspective = cv2.cvtColor(im_gray, cv2.COLOR_GRAY2BGR)
-    #doc_im_contour = cv2.cvtColor(im_gray, cv2.COLOR_GRAY2BGR)
-    ## Doc ##
 
     contour_ids = ignoreContours(im_bw, contours, hierarchy)
     boards = []
     for i in contour_ids:
         c = contours[i]
         c = np.squeeze(c,1)
-
-        ## Doc ##
-        #color = randomColor()
-        #drawContour(doc_im_contour, c, color)
-        ## Doc ##
-
         perspective = getPerspective(img, c)
         if perspective is not None:
             b = extractPerspective(img, perspective, w, h)
             boards.append(b)
-            ## Doc ##
-            #drawPerspective(doc_im_perspective, perspective)
-            ## Doc ##
-
-    ## Doc ##
-    #writeDocumentationImage(boards[-1], "extracted")
-    #writeDocumentationImage(doc_im_contour, "contours_filtered")
-    #writeDocumentationImage(doc_im_perspective, "perspective")
-    ## Doc ##
-
 
     return boards
-
 
 
 def extractGrid(img,
