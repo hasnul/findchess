6ac5549a6968ede50fada85f69463f507fb963e0
bob <hasnul14@gmail.com>
2022-10-23 12:32:35 +0800

    Added future test files

---
+++ tests/test_findchess_contours.py
@@ -0,0 +1,5 @@
+from findchess import Contours
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+
---
+++ tests/test_findchess_quadrangle.py
@@ -0,0 +1,5 @@
+from findchess import Quadrangle
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+
